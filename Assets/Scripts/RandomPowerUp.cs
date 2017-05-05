17fd8df36fbf527fae170a88213e901ce7a21a4f
Daniel Gallardo <aslan_m_d@hotmail.com>
2017-05-05 11:47:50 +0200

    randomPU

---
+++ Assets/Scripts/RandomPowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPowerUp : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	// Use this for initialization
+
+	void Start () {
+		GameObject c = Instantiate (prefabs [Random.Range (0, prefabs.Length)], transform.position, transform.rotation) as GameObject;
+	}
+
+}
