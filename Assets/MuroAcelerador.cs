ac3407bb5e3bc6f43a9233ae8198682f3feff460
Daniel Gallardo <aslan_m_d@hotmail.com>
2017-05-12 12:57:48 +0200

    murosaceleradores y deaceleradores

---
+++ Assets/MuroAcelerador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuroAcelerador : MonoBehaviour {
+
+	public float incremento = 0.0001f;
+	public float velocidad = 1f;
+	
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.CompareTag("Gallina")) {
+			GameManager.instance.frequenciaIncrementoVel = velocidad;
+			GameManager.instance.repetirSubirVelocidad = true;
+			GameManager.instance.incrementoVelocidad = incremento;
+			GameManager.instance.subirVelocidad ();
+		}
+	}
+
+}
