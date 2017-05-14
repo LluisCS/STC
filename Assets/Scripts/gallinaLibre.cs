5cb88e5898978ff91258dde2453d6918e9501af1
Celia <celica02@ucm.es>
2017-05-14 23:32:26 +0200

    Añadido final

---
+++ Assets/Scripts/gallinaLibre.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gallinaLibre : MonoBehaviour {
+    float distancia;
+    float velocidad;
+    float posIni;
+    // Use this for initialization
+    void Start () {
+        distancia = Random.Range (5, 15);
+        velocidad = Random.Range(5, 25);
+        posIni = gameObject.transform.position.x;
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(velocidad * Time.deltaTime, 0, 0));
+        if ((gameObject.transform.position.x >= posIni + distancia) || (gameObject.transform.position.x <= posIni - distancia))
+            gameObject.transform.Rotate(new Vector3(0, gameObject.transform.rotation.y + 180, 0));
+
+
+
+
+
+    }
+}
